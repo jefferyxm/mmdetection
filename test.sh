07317d63924f088e926e866d2da8d693b493cd94
jefferyxm <abc@domain.com>
2019-04-10 10:27:18 +0800

    add .sh file ignore

--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-python setup.py install
-python tools/test.py  configs/maskr50.py work_dirs/mix-0203/epoch_40.pth --show --dataset td900
--- train.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-export CUDA_VISIBLE_DEVICES=0
-python setup.py install
-tools/dist_train.sh configs/maskr50.py 1 --validate
