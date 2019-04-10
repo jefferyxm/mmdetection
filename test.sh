5d20757083dbfafc9853425fd6b36eeea8b226b1
jefferyxm <abc@domain.com>
2019-04-10 10:25:04 +0800

    add ignore of sh file because of problems

--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-python setup.py install
-python tools/test.py  work_dirs/ic15-0303-zhou/maskr50.py work_dirs/ic15-0303-zhou/epoch_60.pth --show --dataset icdar2015
--- train.sh
+++
@@ -1,4 +0,0 @@
-
-#!/usr/bin/env bash
-python setup.py install
-tools/dist_train.sh configs/maskr50.py 1 --validate
